2eafb21fd745e8e97c9ce4ed60973dac9542f066
RRRgy <1277507009@qq.com>
2020-11-27 16:15:16 +0800

    feat:'第一次代码'

---
+++ pctest/src/components/Header/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    header...
+    <p>header child~~~</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="less" scoped>
+.header {
+  background-color: pink;
+  p {
+    font-size: 30px;
+  }
+}
+</style>
---
+++ pctest/src/main.js
@@ -0,0 +1,14 @@
+// @ts-ignore
+import Vue from 'vue';
+// @ts-ignore
+import App from './App';
+import router from "./router";
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+// @ts-ignore
+new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app')

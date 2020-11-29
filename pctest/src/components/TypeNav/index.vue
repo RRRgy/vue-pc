04e2105ce562f7aa28218c11ac8f003357ee5285
RRRgy <1277507009@qq.com>
2020-11-29 21:31:13 +0800

    feat:首页三级分类动态展示

--- pctest/src/components/TypeNav/index.vue
+++ pctest/src/components/TypeNav/index.vue
@@ -15,28 +15,36 @@
       </nav>
       <div class="sort">
         <div class="all-sort-list2">
-          <div class="item bo">
+          <!-- 此时3个item为一级分类名称 -->
+          <!-- 3.遍历数据 -->
+          <div class="item bo" v-for="category in categoryList" :key="category.categoryId">
             <h3>
-              <a href="">图书、音像、数字商品</a>
+              <!-- 4.获取一级分类名称 -->
+              <a href>{{category.categoryName}}</a>
             </h3>
             <div class="item-list clearfix">
               <div class="subitem">
-                <dl class="fore">
+                <!-- 5.遍历二级分类 -->
+                <dl class="fore" v-for="child in category.categoryChild" :key="child.categoryId">
+                  <!-- dt 二级分类名称 -->
                   <dt>
-                    <a href="">电子书</a>
+                    <a href>{{child.categoryName }}</a>
                   </dt>
                   <dd>
-                    <em>
-                      <a href="">婚恋/两性</a>
+                    <!-- em 三级分类名称 -->
+
+                    <!-- 6.遍历三级分类名称 -->
+                    <em v-for="grandChild in child.categoryChild" :key="grandChild.categoryId">
+                      <a href>{{grandChild.categoryName}}</a>
                     </em>
                     <em>
-                      <a href="">文学</a>
+                      <a href>文学</a>
                     </em>
                     <em>
-                      <a href="">经管</a>
+                      <a href>经管</a>
                     </em>
                     <em>
-                      <a href="">畅读VIP</a>
+                      <a href>畅读VIP</a>
                     </em>
                   </dd>
                 </dl>
@@ -45,79 +53,79 @@
           </div>
           <div class="item">
             <h3>
-              <a href="">家用电器</a>
+              <a href>家用电器</a>
             </h3>
             <div class="item-list clearfix">
               <div class="subitem">
                 <dl class="fore">
                   <dt>
-                    <a href="">电子书1</a>
+                    <a href>电子书1</a>
                   </dt>
                   <dd>
                     <em>
-                      <a href="">免费</a>
+                      <a href>免费</a>
                     </em>
                     <em>
-                      <a href="">小说</a>
+                      <a href>小说</a>
                     </em>
                     <em>
-                      <a href="">励志与成功</a>
+                      <a href>励志与成功</a>
                     </em>
                     <em>
-                      <a href="">婚恋/两性</a>
+                      <a href>婚恋/两性</a>
                     </em>
                     <em>
-                      <a href="">文学</a>
+                      <a href>文学</a>
                     </em>
                     <em>
-                      <a href="">经管</a>
+                      <a href>经管</a>
                     </em>
                     <em>
-                      <a href="">畅读VIP</a>
+                      <a href>畅读VIP</a>
                     </em>
                   </dd>
                 </dl>
                 <dl class="fore">
                   <dt>
-                    <a href="">数字音乐</a>
+                    <a href>数字音乐</a>
                   </dt>
                   <dd>
                     <em>
-                      <a href="">通俗流行</a>
+                      <a href>通俗流行</a>
                     </em>
                     <em>
-                      <a href="">古典音乐</a>
+                      <a href>古典音乐</a>
                     </em>
                     <em>
-                      <a href="">摇滚说唱</a>
+                      <a href>摇滚说唱</a>
                     </em>
                     <em>
-                      <a href="">爵士蓝调</a>
+                      <a href>爵士蓝调</a>
                     </em>
                     <em>
-                      <a href="">乡村民谣</a>
+                      <a href>乡村民谣</a>
                     </em>
                     <em>
-                      <a href="">有声读物</a>
+                      <a href>有声读物</a>
                     </em>
                   </dd>
                 </dl>
                 <dl class="fore">
                   <dt>
-                    <a href="">音像</a>
+                    <a href>音像</a>
                   </dt>
                   <dd>
                     <em>
-                      <a href="">音乐</a>
+                      <a href>音乐</a>
                     </em>
                     <em>
-                      <a href="">影视</a>
+                      <a href>影视</a>
                     </em>
                     <em>
-                      <a href="">教育音像</a>
+                      <a href>教育音像</a>
                     </em>
                     <em>
-                      <a href="">游戏</a>
+                      <a href>游戏</a>
                     </em>
                   </dd>
                 </dl>
@@ -125,19 +133,19 @@
                   <dt>文艺</dt>
                   <dd>
                     <em>
-                      <a href="">小说</a>
+                      <a href>小说</a>
                     </em>
                     <em>
-                      <a href="">文学</a>
+                      <a href>文学</a>
                     </em>
                     <em>
-                      <a href="">青春文学</a>
+                      <a href>青春文学</a>
                     </em>
                     <em>
-                      <a href="">传记</a>
+                      <a href>传记</a>
                     </em>
                     <em>
-                      <a href="">艺术</a>
+                      <a href>艺术</a>
                     </em>
                   </dd>
                 </dl>
@@ -145,22 +153,22 @@
                   <dt>人文社科</dt>
                   <dd>
                     <em>
-                      <a href="">历史</a>
+                      <a href>历史</a>
                     </em>
                     <em>
-                      <a href="">心理学</a>
+                      <a href>心理学</a>
                     </em>
                     <em>
-                      <a href="">政治/军事</a>
+                      <a href>政治/军事</a>
                     </em>
                     <em>
-                      <a href="">国学/古籍</a>
+                      <a href>国学/古籍</a>
                     </em>
                     <em>
-                      <a href="">哲学/宗教</a>
+                      <a href>哲学/宗教</a>
                     </em>
                     <em>
-                      <a href="">社会科学</a>
+                      <a href>社会科学</a>
                     </em>
                   </dd>
                 </dl>
@@ -168,16 +176,16 @@
                   <dt>经管励志</dt>
                   <dd>
                     <em>
-                      <a href="">经济</a>
+                      <a href>经济</a>
                     </em>
                     <em>
-                      <a href="">金融与投资</a>
+                      <a href>金融与投资</a>
                     </em>
                     <em>
-                      <a href="">管理</a>
+                      <a href>管理</a>
                     </em>
                     <em>
-                      <a href="">励志与成功</a>
+                      <a href>励志与成功</a>
                     </em>
                   </dd>
                 </dl>
@@ -185,34 +193,34 @@
                   <dt>生活</dt>
                   <dd>
                     <em>
-                      <a href="">家庭与育儿</a>
+                      <a href>家庭与育儿</a>
                     </em>
                     <em>
-                      <a href="">旅游/地图</a>
+                      <a href>旅游/地图</a>
                     </em>
                     <em>
-                      <a href="">烹饪/美食</a>
+                      <a href>烹饪/美食</a>
                     </em>
                     <em>
-                      <a href="">时尚/美妆</a>
+                      <a href>时尚/美妆</a>
                     </em>
                     <em>
-                      <a href="">家居</a>
+                      <a href>家居</a>
                     </em>
                     <em>
-                      <a href="">婚恋与两性</a>
+                      <a href>婚恋与两性</a>
                     </em>
                     <em>
-                      <a href="">娱乐/休闲</a>
+                      <a href>娱乐/休闲</a>
                     </em>
                     <em>
-                      <a href="">健身与保健</a>
+                      <a href>健身与保健</a>
                     </em>
                     <em>
-                      <a href="">动漫/幽默</a>
+                      <a href>动漫/幽默</a>
                     </em>
                     <em>
-                      <a href="">体育/运动</a>
+                      <a href>体育/运动</a>
                     </em>
                   </dd>
                 </dl>
@@ -220,28 +228,28 @@
                   <dt>科技</dt>
                   <dd>
                     <em>
-                      <a href="">科普</a>
+                      <a href>科普</a>
                     </em>
                     <em>
-                      <a href="">IT</a>
+                      <a href>IT</a>
                     </em>
                     <em>
-                      <a href="">建筑</a>
+                      <a href>建筑</a>
                     </em>
                     <em>
-                      <a href="">医学</a>
+                      <a href>医学</a>
                     </em>
                     <em>
-                      <a href="">工业技术</a>
+                      <a href>工业技术</a>
                     </em>
                     <em>
-                      <a href="">电子/通信</a>
+                      <a href>电子/通信</a>
                     </em>
                     <em>
-                      <a href="">农林</a>
+                      <a href>农林</a>
                     </em>
                     <em>
-                      <a href="">科学与自然</a>
+                      <a href>科学与自然</a>
                     </em>
                   </dd>
                 </dl>
@@ -249,19 +257,19 @@
                   <dt>少儿</dt>
                   <dd>
                     <em>
-                      <a href="">少儿</a>
+                      <a href>少儿</a>
                     </em>
                     <em>
-                      <a href="">0-2岁</a>
+                      <a href>0-2岁</a>
                     </em>
                     <em>
-                      <a href="">3-6岁</a>
+                      <a href>3-6岁</a>
                     </em>
                     <em>
-                      <a href="">7-10岁</a>
+                      <a href>7-10岁</a>
                     </em>
                     <em>
-                      <a href="">11-14岁</a>
+                      <a href>11-14岁</a>
                     </em>
                   </dd>
                 </dl>
@@ -270,13 +278,13 @@
           </div>
           <div class="item">
             <h3>
-              <a href="">手机、数码、充值</a>
+              <a href>手机、数码、充值</a>
             </h3>
             <div class="item-list clearfix">
               <div class="subitem">
                 <dl class="fore">
                   <dt>
-                    <a href="">充值</a>
+                    <a href>充值</a>
                   </dt>
                   <dd>
                     <em>
@@ -286,73 +294,73 @@
                 </dl>
                 <dl class="fore">
                   <dt>
-                    <a href="">电子书2</a>
+                    <a href>电子书2</a>
                   </dt>
                   <dd>
                     <em>
-                      <a href="">免费</a>
+                      <a href>免费</a>
                     </em>
                     <em>
-                      <a href="">小说</a>
+                      <a href>小说</a>
                     </em>
                     <em>
-                      <a href="">励志与成功</a>
+                      <a href>励志与成功</a>
                     </em>
                     <em>
-                      <a href="">婚恋/两性</a>
+                      <a href>婚恋/两性</a>
                     </em>
                     <em>
-                      <a href="">文学</a>
+                      <a href>文学</a>
                     </em>
                     <em>
-                      <a href="">经管</a>
+                      <a href>经管</a>
                     </em>
                     <em>
-                      <a href="">畅读VIP</a>
+                      <a href>畅读VIP</a>
                     </em>
                   </dd>
                 </dl>
                 <dl class="fore">
                   <dt>
-                    <a href="">数字音乐</a>
+                    <a href>数字音乐</a>
                   </dt>
                   <dd>
                     <em>
-                      <a href="">通俗流行</a>
+                      <a href>通俗流行</a>
                     </em>
                     <em>
-                      <a href="">古典音乐</a>
+                      <a href>古典音乐</a>
                     </em>
                     <em>
-                      <a href="">摇滚说唱</a>
+                      <a href>摇滚说唱</a>
                     </em>
                     <em>
-                      <a href="">爵士蓝调</a>
+                      <a href>爵士蓝调</a>
                     </em>
                     <em>
-                      <a href="">乡村民谣</a>
+                      <a href>乡村民谣</a>
                     </em>
                     <em>
-                      <a href="">有声读物</a>
+                      <a href>有声读物</a>
                     </em>
                   </dd>
                 </dl>
                 <dl class="fore">
                   <dt>
-                    <a href="">音像</a>
+                    <a href>音像</a>
                   </dt>
                   <dd>
                     <em>
-                      <a href="">音乐</a>
+                      <a href>音乐</a>
                     </em>
                     <em>
-                      <a href="">影视</a>
+                      <a href>影视</a>
                     </em>
                     <em>
-                      <a href="">教育音像</a>
+                      <a href>教育音像</a>
                     </em>
                     <em>
-                      <a href="">游戏</a>
+                      <a href>游戏</a>
                     </em>
                   </dd>
                 </dl>
@@ -360,19 +368,19 @@
                   <dt>文艺</dt>
                   <dd>
                     <em>
-                      <a href="">小说</a>
+                      <a href>小说</a>
                     </em>
                     <em>
-                      <a href="">文学</a>
+                      <a href>文学</a>
                     </em>
                     <em>
-                      <a href="">青春文学</a>
+                      <a href>青春文学</a>
                     </em>
                     <em>
-                      <a href="">传记</a>
+                      <a href>传记</a>
                     </em>
                     <em>
-                      <a href="">艺术</a>
+                      <a href>艺术</a>
                     </em>
                   </dd>
                 </dl>
@@ -380,22 +388,22 @@
                   <dt>人文社科</dt>
                   <dd>
                     <em>
-                      <a href="">历史</a>
+                      <a href>历史</a>
                     </em>
                     <em>
-                      <a href="">心理学</a>
+                      <a href>心理学</a>
                     </em>
                     <em>
-                      <a href="">政治/军事</a>
+                      <a href>政治/军事</a>
                     </em>
                     <em>
-                      <a href="">国学/古籍</a>
+                      <a href>国学/古籍</a>
                     </em>
                     <em>
-                      <a href="">哲学/宗教</a>
+                      <a href>哲学/宗教</a>
                     </em>
                     <em>
-                      <a href="">社会科学</a>
+                      <a href>社会科学</a>
                     </em>
                   </dd>
                 </dl>
@@ -409,9 +417,21 @@
 </template>
 
 <script>
+import { getBaseCategoryList } from "@api/home";
+
 export default {
-  name: 'TypeNav',
-}
+  name: "TypeNav",
+  data() {
+    return {
+      //2.此时先初始化一个响应式数据
+      categoryList: [],
+    };
+  },
+  async mounted() {
+    const result = await getBaseCategoryList(); //1.此时更新响应式数据希望用户界面也变化所以定义data
+    this.categoryList = result;
+  },
+};
 </script>
 
 <style  lang="less" scoped>
